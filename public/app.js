4385e56c6d9eb8db877e31cd2fd0d75d136d2d1d
James Quigley <jmquigley@outlook.com>
2017-09-19 17:08:22 -0500

    Adds missing app.js file to git
    Caused by .gitignore rule for .js files (as generally only .ts files
    need to be saved in this project).

---
+++ public/app.js
@@ -0,0 +1,32 @@
+Quill.register('modules/markup', Markup);
+let quill = new Quill('#editor', {
+	modules: {
+		markup: {
+			mode: MarkupMode.markdown,
+			styling: MarkupStyle.monokai
+		},
+		toolbar: false
+	},
+	theme: 'snow'
+});
+
+let markup = quill.getModule('markup');
+markup.set({
+	content: 'Hello World'
+});
+
+document.getElementById("font").onchange = (e) => {
+	markup.setFont(e.target.value);
+}
+
+document.getElementById("fontsize").onchange = (e) => {
+	markup.setFontSize(e.target.value);
+}
+
+document.getElementById("bold-button").onclick = (e) => {
+	markup.setBold();
+}
+
+document.getElementById("italic-button").onclick = (e) => {
+	markup.setItalic();
+}
